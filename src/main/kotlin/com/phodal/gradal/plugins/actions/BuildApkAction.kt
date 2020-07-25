d8e6b2bdb83b8e75f9ffaba0d277c6ee427ea677
Phodal Huang <h@phodal.com>
2020-07-25 19:28:03 +0800

    refactor: emove unused logic for testing

--- src/main/kotlin/com/phodal/gradal/plugins/actions/BuildApkAction.kt
+++ src/main/kotlin/com/phodal/gradal/plugins/actions/BuildApkAction.kt
@@ -2,15 +2,9 @@ package com.phodal.gradal.plugins.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
-import com.intellij.openapi.project.Project
 import com.phodal.gradal.plugins.gradle.GradleTasksExecutorImpl
-import com.phodal.gradal.plugins.gradle.facet.GradleFacet
 import com.phodal.gradal.plugins.gradle.run.OutputBuildActionUtil
-import java.util.*
-import java.util.concurrent.ConcurrentLinkedQueue
-import java.util.stream.Collectors
 
 
 class BuildApkAction : AnAction() {
@@ -24,31 +18,10 @@ class BuildApkAction : AnAction() {
 //        toolWindow.contentManager.addContent(content)
 //        consoleView.print("Hello from MyPlugin!", ConsoleViewContentType.NORMAL_OUTPUT)
 
-        val appModules = analyzeProjectStructure(project)
-                .stream()
-                .collect(Collectors.toList());
-
         val moduleManager = ModuleManager.getInstance(project)
         val modules = moduleManager.modules
         val buildAction = OutputBuildActionUtil.create(modules);
 
         GradleTasksExecutorImpl().executeTask(project, projectPath, buildAction)
-    }
-
-    private fun analyzeProjectStructure(myProject: Project): Queue<Module> {
-        val appModules: Queue<Module> = ConcurrentLinkedQueue()
-
-        val moduleManager = ModuleManager.getInstance(myProject)
-        val modules = moduleManager.modules
-
-        for (module in modules) {
-            val gradleFacet: GradleFacet? = GradleFacet.getInstance(module)
-            if (gradleFacet != null) {
-                val gradlePath = gradleFacet.configuration.GRADLE_PROJECT_PATH
-                appModules.add(module)
-            }
-        }
-
-        return appModules
     }
 }
